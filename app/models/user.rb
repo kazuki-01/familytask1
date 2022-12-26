51765a7b2a8ed319aec8f86e3b83e8f984f2ac25
Kazuki.M <kazuki0922m@gmail.com>
2022-12-26 23:36:52 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
 
-  has_secure_password
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+   has_secure_password  #モデルへのパスワード暗号化機能の実装のコード（bcryp）
+
+   validates :name, presence: true   #空白は許可しない
+   validates :email, presence: true ,uniqueness: true  #空白は許可しない  #被らない
+   validates :password, presence: true,   #空白は許可しない
+   length: {minimum: 6}   #最低６文字必要とする
+
 end
