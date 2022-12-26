0820e6aa05e5d8f376c88520c9f1e33fe3369d26
Kazuki.M <kazuki0922m@gmail.com>
2022-12-27 00:20:14 +0900

    ユーザー登録・チェック・詳細・編集画面のページ作成
    rails g controller users show new edit check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/check'
+
+ 
 end
