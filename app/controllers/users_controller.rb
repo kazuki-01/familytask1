6623a3e1255c44c07495632cba060d58cb7af16b
Kazuki.M <kazuki0922m@gmail.com>
2023-01-02 02:37:59 +0900

    new,chek,createメソッドを記述。 ストロングパラメーターを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,37 @@
 class UsersController < ApplicationController
-  def show
-  end
+
 
   def new
+    @user = User.new
+  end
+
+  def check
+    @user = User.new(image: params[:プロフィールアイコン], name: params[:お名前], email: params[:メールアドレス], password: params[:パスワード])
+  end
+
+  def create
+    @user = User.new(image: params[:プロフィールアイコン], name: params[:お名前], email: params[:メールアドレス], password: params[:パスワード])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path, notice: "新規登録が完了しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
-  def check
+  def show
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image,:name, :email, :password)
   end
   
 end
