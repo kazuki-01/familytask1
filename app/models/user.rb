9b26ab2604e459f3f247e3fedc8e825bfc635256
Kazuki.M <kazuki0922m@gmail.com>
2023-01-02 01:59:53 +0900

    テーブルに画像ファイルを紐付けする
    has_one_attachedメソッド
    各メソッドとファイルを１対１で紐付けるメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :image
 
-   has_secure_password  #モデルへのパスワード暗号化機能の実装のコード（bcryp）
 
-   validates :name, presence: true   #空白は許可しない
-   validates :email, presence: true ,uniqueness: true  #空白は許可しない  #被らない
-   validates :password, presence: true,   #空白は許可しない
-   length: {minimum: 6}   #最低６文字必要とする
+  has_secure_password  #モデルへのパスワード暗号化機能の実装のコード（bcryp）
+
+  validates :name, presence: true   #空白は許可しない
+  validates :email, presence: true ,uniqueness: true  #空白は許可しない  #被らない
+  validates :password, presence: true,   #空白は許可しない
+  length: {minimum: 6}   #最低６文字必要とする
 
 end
