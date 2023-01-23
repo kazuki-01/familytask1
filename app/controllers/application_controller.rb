2497fd7fc10afec0e394be31ab4b9b5a88b5c7fe
Kazuki.M <kazuki0922m@gmail.com>
2023-01-23 16:13:22 +0900

    current_user を使えるようにする 参考資料 https://qiita.com/you8/items/a98596547c12d844c53d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user     #current_user(今ログインしているユーザー)のIDをviewでも使えるようになる
+
   
 
   def after_signup_path_for(resource)
     check_path
+  end
+
+  def current_user
+    if session[:user_id]          #session　IDが、nilではなかったら、current_userにsession　IDを入れる
+        current_user ||= User.find(session[:user_id])
+    end
   end
 
 
